ccd1f738bc332b00086bee288e5128255126f673
Raphaël Bonnet <harcyah@gmail.com>
2020-09-14 23:13:00 +0200

    Bump to junit5

--- src/test/java/com/harcyah/sample/wsdl_to_java/AddBookTest.java
+++ src/test/java/com/harcyah/sample/wsdl_to_java/AddBookTest.java
@@ -1,10 +1,10 @@
 package com.harcyah.sample.wsdl_to_java;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AddBookTest {
+class AddBookTest {
 
     @Test
     public void testAddBookHasNoArgsConstructor() {
